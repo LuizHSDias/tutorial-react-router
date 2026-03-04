efd577280878cc41331fbf7c3799c8629c90a869
LHSDias <luizhenriquemc15@gmail.com>
2026-03-04 16:16:24 -0300

    Rota Não Encontrada

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,21 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Expenses from './routes/Expenses/index.js'
-import Invoices from './routes/Invoices/index.js'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Expenses from "./routes/Expenses/index.js";
+import Invoices from "./routes/Invoices/index.js";
+import NotFound from "./routes/NotFound/index.js";
 
-
-createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
-      <Route path="expenses" element={<Expenses />} />
-      <Route path="invoices" element={<Invoices />} />
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />} />
+        <Route
+          path="*" element={<NotFound />}
+        />
       </Route>
     </Routes>
-  </BrowserRouter>
-  
-)
+  </BrowserRouter>,
+);
