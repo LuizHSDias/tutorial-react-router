8566408aec716d5b7f490c0e7a57cce3fe4b7703
LHSDias <luizhenriquemc15@gmail.com>
2026-03-04 15:42:00 -0300

    Nested routes

--- src/main.jsx
+++ src/main.jsx
@@ -9,9 +9,10 @@ import Invoices from './routes/Invoices/index.js'
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<App />}>
       <Route path="expenses" element={<Expenses />} />
       <Route path="invoices" element={<Invoices />} />
+      </Route>
     </Routes>
   </BrowserRouter>
   
